96a82bfa800191ba63c0b2b7843dfa50df05c82a
GabrielNeimester <115425284+GabrielNeimester@users.noreply.github.com>
2024-03-24 16:22:12 -0300

    correct app variable

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,10 @@ import routes from './routes'
 import cors from 'cors'
 
 dotenv.config()
-app.use(cors());
+
 const app = express()
 const port = process.env.PORT || 5173 || 3001
+app.use(cors());
 
 app.use(express.json()) // habilita o express para receber dados no formato json
 app.use(routes) // habilita as rotas
